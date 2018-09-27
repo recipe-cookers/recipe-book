4bb8147b27063d7150d51c1f078cc708e707a153
Entrant <trueentrant@gmail.com>
2018-09-27 18:38:33 +0300

    very start of start page

--- src/app/start/start.module.ts
+++ src/app/start/start.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartComponent } from './start/start.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [StartComponent],
   exports: [StartComponent]
