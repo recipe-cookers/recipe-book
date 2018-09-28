b57a036b95cb5450f3e6e408262f34b61db3fa96
lavoide <lavoidde@gmail.com>
2018-09-29 00:13:09 +0300

    added recipe service/ filtering recipes by id for showing details, finished routing for recipe item

--- src/app/recipes/item/item.component.ts
+++ src/app/recipes/item/item.component.ts
@@ -1,5 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IRecipe} from '../../shared/models/recipe.model';
+import {Observable} from 'rxjs';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {RecipeService} from '../../shared/services/recipe.service';
+import {switchMap} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'recipe-item',
@@ -7,10 +11,22 @@ import {IRecipe} from '../../shared/models/recipe.model';
   styleUrls: ['./item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
-  constructor() { }
-  @Input() recipe: IRecipe;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: RecipeService
+  ) { }
+  recipe$: Observable<IRecipe>;
   ngOnInit() {
-    console.log(this.recipe);
+    this.recipe$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.service.getRecipe(params.get('id')))
+    );
   }
-
+  gotoRecipeList(recipe: IRecipe) {
+    const recipeId = recipe ? recipe.id : null;
+    // Pass along the recipe id if available
+    // so that the RecipeList component can select that recipe.
+    this.router.navigate(['/recipies', { id: recipeId}]);
+  }
 }
--- src/app/recipes/list/list.component.ts
+++ src/app/recipes/list/list.component.ts
@@ -1,7 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {IRecipe} from '../../shared/models/recipe.model';
-import {Router} from '@angular/router';
-
+import {recipies} from '../../../mock' ;
 
 @Component({
   selector: 'recipe-list',
@@ -11,24 +9,14 @@ import {Router} from '@angular/router';
 export class RecipeListComponent implements OnInit {
 
   constructor() { }
-  public ItemRecipe: IRecipe[] = [
-    {
-      id: 1,
-      name: 'Fried Potato',
-      description: 'Картошку берите крупную и крахмалистую. Крахмал при тепловой обработке даст сладость, которая очень гармонирует с грибами. Нередко для жарки используется менее крахмалистый картофель, потому что он не рассыпается. Но крахмалистая вкуснее, и надо просто готовить ее особым образом — увидите как.\n' +
-      '\n' +
-      'Вы можете нарезать картошку как хотите. Расскажу, как делаю я. Срезаю с двух длинных сторон первые, скругленные сверху ломтики, и откладываю их: теперь картошку можно поставить на длинный срез и она не будет кататься. Затем я нарезаю ее длинными брусками с палец толщиной, на манер картошки фри.\n' +
-      '\n' +
-      'Рассуждать о пропорциях картофеля и грибов сложно, все делают, как любят, в зависимости от бюджета или от того, сколько грибов собрали в лесу. Я бы взял картофеля в два раза больше, чем грибов, исходя из их сухого веса.',
-      image: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/11/2/0/DV1510H_fried-chicken-recipe-10_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371611618540.jpeg',
-      ingridients: [{id: 1, name: 'Kartoshka', amount: 4, measure: 'kruzhka'}]
-    }
-  ];
+  public recipies = recipies;
   @Output() buttonPressed = new EventEmitter<Object>();
+  // search id from all recipes func
   clickHandler(id) {
-    this.buttonPressed.emit(this.ItemRecipe.filter(x => x.id === id));
+    this.buttonPressed.emit(this.recipies.filter(x => x.id === id));
   }
   ngOnInit() {
+    console.log(recipies);
   }
 
 }
