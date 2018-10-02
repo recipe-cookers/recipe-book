f18549dbb90749fc43461827e54d4d70a181cc0c
Alex0382 <lex030382@gmail.com>
2018-10-02 19:47:39 +0300

    ingridients commit

--- src/app/ingridients/ingridients.module.ts
+++ src/app/ingridients/ingridients.module.ts
@@ -4,12 +4,20 @@ import { IngridientsListComponent } from './list/list.component';
 import { IngridientsItemComponent } from './item/item.component';
 import { IngridientsFormComponent } from './form/form.component';
 import { IngridientsApplyComponent } from './apply/apply.component';
+import { IngridientRouterModule } from './ingridients-routing.module';
+import { ListGuard } from './list/guard/list-guard.service';
+import { FormsModule }   from '@angular/forms';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    IngridientRouterModule,
+    FormsModule,
+    
   ],
   declarations: [IngridientsListComponent, IngridientsItemComponent, IngridientsFormComponent, IngridientsApplyComponent],
-  exports: [IngridientsListComponent, IngridientsItemComponent, IngridientsFormComponent, IngridientsApplyComponent]
+  exports: [IngridientsListComponent, IngridientsItemComponent, IngridientsFormComponent, IngridientsApplyComponent],
+  providers: [ ListGuard]
 })
 export class IngridientsModule { }
