d117698ef1fa9fab211b931c763b86cc4c32d1e4
lavoide <lavoidde@gmail.com>
2018-10-02 15:53:51 +0300

    added new componets and coonected to routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  exports: [RegistrationComponent, LoginComponent, ProfileComponent],
+  declarations: [RegistrationComponent, LoginComponent, ProfileComponent]
 })
 export class CoreModule { }
