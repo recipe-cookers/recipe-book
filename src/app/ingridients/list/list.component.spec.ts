af1d27c2dc2985322687ad5512209500b148f5eb
Alanahh <alonavasylenko1996@gmail.com>
2018-10-19 20:36:03 +0300

    ingridients-component pre-style proposal

--- src/app/ingridients/list/list.component.spec.ts
+++ src/app/ingridients/list/list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ListComponent } from './list.component';
+import { IngridientsListComponent } from './list.component';
 
-describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+describe('IngridientsListComponent', () => {
+  let component: IngridientsListComponent;
+  let fixture: ComponentFixture<IngridientsListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
+      declarations: [ IngridientsListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListComponent);
+    fixture = TestBed.createComponent(IngridientsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
