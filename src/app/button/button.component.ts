15f3d944eb1a0abbd8e8b300e6991ec6861a15dc
Alanahh <alonavasylenko1996@gmail.com>
2018-10-19 17:18:45 +0300

    nav-component 0.03

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-button',
@@ -6,11 +7,30 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
-  @Input() data: {};
-  
-  constructor() { }
+  @Input() data: {name: string, src1: string, src2: string, url: string,id:number};
+  // @Input() data : {};
+  display1 = 'active';
+  display2 = 'none';
+  display3 = 'text1';
+
+  onMouseOver(){
+    this.display1 = 'none';
+    this.display2 = 'active';
+    this.display3='';
+  }
+  onMouseOut(){
+ 
+    this.display1 = 'active';
+    this.display2 = 'none';
+    this.display3 = 'text1';
+    
+  }
+
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+   
   }
 
 }
