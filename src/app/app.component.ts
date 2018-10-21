6c956cfdf427bf9db68fc43d51b775718388c124
lavoide <lavoidde@gmail.com>
2018-10-21 15:28:36 +0300

    login styling done?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'recipe-book';
+export class AppComponent implements OnInit, OnChanges {
+  ngOnInit() {
+    console.log('app works');
+    console.log(this.router.url);
+  }
+  ngOnChanges() {
+    console.log(this.router.url);
+  }
+  constructor(private router: Router) {}
 }
